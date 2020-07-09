f9e91656fc55a383db76ccb047dbe1064593662c
Israel Canessa <israellcanessa@gmail.com>
2020-07-09 00:13:19 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,12 +1,12 @@
 def count_strings(array)
-  array.count do { |index| index == ""}
+  array.count do { |index| index == "" }
 
   # Return the total number of strings in the provided array using the count enumerable
 
 end
 
 def count_empty_strings(array)
-  
+
 
   # Return the total number of EMPTY strings in the provided array using the count enumerable
 end
